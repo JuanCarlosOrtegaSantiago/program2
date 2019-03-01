ebe01b8a4d5751a09662dfa3887d4947f3881e07
Juan Carlos Ortega Santiago <tecprogjuancarlos@gmail.com>
2019-03-01 01:18:08 -0600

    Programa mostrar numeros de un arreglo

--- program2/Program.cs
+++ program2/Program.cs
@@ -10,6 +10,22 @@ namespace program2
     {
         static void Main(string[] args)
         {
+            int[] arreglo = new int[25];
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine("Ingresa el dato {0}", i + 1);
+                arreglo[i] = int.Parse(Console.ReadLine());
+            }
+
+            int suma = 0;
+            Console.Clear();
+            foreach (var item in arreglo)
+            {
+                Console.WriteLine(item);
+                suma += item;
+            }
+            Console.WriteLine("La suma es de: {0}",suma);
+            Console.ReadLine();
         }
     }
 }
